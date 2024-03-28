3bd6536abeb5acd5f4cafe49c57748096e22cb43
ValentinaSukonina <v.trana@icloud.com>
2024-03-28 20:19:23 +0100

    UserRepositoryTest completed, Table name changed to usertable in UserEntity

---
+++ src/test/java/dev/ebrydeu/spring_boot_library/repositrories/MessageRepositoryTest.java
@@ -0,0 +1,7 @@
+package dev.ebrydeu.spring_boot_library.repositrories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageRepositoryTest {
+
+}
---
+++ src/test/java/dev/ebrydeu/spring_boot_library/repositrories/UserRepositoryTest.java
@@ -0,0 +1,74 @@
+package dev.ebrydeu.spring_boot_library.repositrories;
+
+import dev.ebrydeu.spring_boot_library.domain.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("dev")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    private User savedUser;
+
+    @BeforeEach
+    void setUp() {
+        User user = new User();
+        user.setProfileName("svenX");
+        user.setFirstName("Sven");
+        user.setLastName("Svensson");
+        user.setProfilePicture("profile_picture_1");
+        user.setEmail("svensson@example.com");
+
+        savedUser = entityManager.persist(user);
+        entityManager.flush();
+    }
+
+    @Test
+    void saveNewUserToDatabaseSuccessful() {
+
+        assertThat(entityManager.find(User.class, savedUser.getId())).isEqualTo(savedUser);
+    }
+
+    @Test
+    void deleteUserFromDatabaseSuccessful() {
+        userRepository.delete(savedUser);
+
+        assertThat(entityManager.find(User.class, savedUser.getId())).isNull();
+    }
+
+    @Test
+    void findByProfileNameSuccessful() {
+        List<User> retrievedUser = userRepository.findByProfileName("svenX");
+        assertThat(retrievedUser).contains(savedUser);
+    }
+
+    @Test
+    void findByFirstNameSuccessful() {
+        List<User> retrievedUser = userRepository.findByFirstName("Sven");
+        assertThat(retrievedUser).contains(savedUser);
+    }
+
+    @Test
+    void findByLastNameSuccessful() {
+        List<User> retrievedUser = userRepository.findByLastName("Svensson");
+        assertThat(retrievedUser).contains(savedUser);
+    }
+
+    @Test
+    void findByEmailSuccessful() {
+        List<User> retrievedUser = userRepository.findByEmail("svensson@example.com");
+        assertThat(retrievedUser).contains(savedUser);
+    }
+}
