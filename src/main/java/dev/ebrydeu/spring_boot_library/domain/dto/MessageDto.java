8c38c5961e65a8afa17ae2d45d0b11802aef3191
Adam Johnsson <ajohnsson95@hotmail.com>
2024-04-02 15:21:43 +0200

    Add Spring boot starter validation dependency
    Validtaions added in MessageDto
    & UserDto

--- src/main/java/dev/ebrydeu/spring_boot_library/domain/dto/MessageDto.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/domain/dto/MessageDto.java
@@ -1,15 +1,26 @@
 package dev.ebrydeu.spring_boot_library.domain.dto;
 
 import dev.ebrydeu.spring_boot_library.domain.entities.Message;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 import java.time.Instant;
 
 public record MessageDto(
         Long id,
+        @NotBlank(message = "Title must not be blank")
+        @Size(max = 100, message = "Title must not exceed 100 characters")
         String title,
+        @NotBlank(message = "Body must not be blank")
         String body,
+        @NotBlank(message = "Author must not be blank")
         String author,
+        @NotNull(message = "Date must not be null")
+        @PastOrPresent(message = "Date must be in the past or present")
         Instant date,
+        @NotNull(message = "User information must not be null")
         UserDto user) {
 
     public static MessageDto map(Message entity) {
@@ -25,12 +36,12 @@ public record MessageDto(
 
     public static Message map(MessageDto dto) {
         Message message = new Message();
-        message.setId(dto.id);
-        message.setTitle(dto.title);
-        message.setBody(dto.body);
-        message.setAuthor(dto.author);
-        message.setDate(dto.date);
-        message.setUser(UserDto.map(dto.user));
+        message.setId(dto.id());
+        message.setTitle(dto.title());
+        message.setBody(dto.body());
+        message.setAuthor(dto.author());
+        message.setDate(dto.date());
+        message.setUser(UserDto.map(dto.user()));
 
         return message;
     }
