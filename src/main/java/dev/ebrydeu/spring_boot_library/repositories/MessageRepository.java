2a54661309a715406d9b150ff4597aae8fc0b1f9
Adam Johnsson <ajohnsson95@hotmail.com>
2024-04-04 12:13:16 +0200

    New testpush
    Co-authored-by: name <Charlotte.Henriksson@iths.se>

--- src/main/java/dev/ebrydeu/spring_boot_library/repositories/MessageRepository.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/repositories/MessageRepository.java
@@ -1,13 +1,13 @@
 package dev.ebrydeu.spring_boot_library.repositories;
 
 import dev.ebrydeu.spring_boot_library.domain.entities.Message;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MessageRepository extends ListCrudRepository<Message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByTitle(String title);
 
 }
--- src/main/java/dev/ebrydeu/spring_boot_library/repositories/UserRepository.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/repositories/UserRepository.java
@@ -1,14 +1,14 @@
 package dev.ebrydeu.spring_boot_library.repositories;
 
 import dev.ebrydeu.spring_boot_library.domain.entities.User;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends ListCrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUsername(String username);
 
     List<User> findByFirstname(String firstname);
