ef7e26a2a049bc34e91f25d703dd7e94b54e92f6
Adam Johnsson <ajohnsson95@hotmail.com>
2024-04-04 08:57:15 +0200

    Setup Simple AudtorAware to se if test pass until security is setup in the project
    Co-authored-by: name <Charlotte.Henriksson@iths.se>

--- src/main/java/dev/ebrydeu/spring_boot_library/config/AuditorConfig.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/config/AuditorConfig.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class AuditorConfig {
-    //@Bean
-    //public AuditorAware<String> auditorProvider() {
+    @Bean
+    public AuditorAware<String> auditorProvider() {
         //todo return new SpringSecurityAuditorAware();
-    //}
+    return () -> Optional.of("admin");}
 }
