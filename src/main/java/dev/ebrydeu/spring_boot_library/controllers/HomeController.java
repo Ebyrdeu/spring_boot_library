a96702cd2aed318bd573a8eb7f0d536ffb81d022
ValentinaSukonina <v.trana@icloud.com>
2024-04-05 18:57:02 +0200

    added home.html, index.html and HomeController, oauth2 is disabled in pom.xml

--- src/main/java/dev/ebrydeu/spring_boot_library/controllers/HomeController.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/home")
-    public String showHomeForm() {
+    public String home() {
         return "home";
     }
 }
