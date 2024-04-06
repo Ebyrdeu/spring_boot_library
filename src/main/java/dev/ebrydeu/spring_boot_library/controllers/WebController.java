a3bc131277765077af17cf28363655f15ab0f98f
Adam Johnsson <ajohnsson95@hotmail.com>
2024-04-06 14:58:34 +0200

    Add @GetMapping ("/guest-page") and changed name from HomeController -> WebController
    Co-authored-by: name <Charlotte.Henriksson@iths.se>

--- src/main/java/dev/ebrydeu/spring_boot_library/controllers/WebController.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/controllers/WebController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class WebController {
 
     @GetMapping("/home")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/guest-page")
+    public String guestPage() {return "guest-page";}
 }
+
