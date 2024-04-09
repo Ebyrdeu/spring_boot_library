3a1f9fe2b2beb2963405ff61c70eb7ad65a2728b
ValentinaSukonina <v.trana@icloud.com>
2024-04-09 08:53:40 +0200

    added dependency spring-boot-starter-cache and @EnableCaching

--- src/main/java/dev/ebrydeu/spring_boot_library/Application.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/Application.java
@@ -2,8 +2,10 @@ package dev.ebrydeu.spring_boot_library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
