2e88e2ae0913e15434eb980aaf34168764e35902
Adam Johnsson <ajohnsson95@hotmail.com>
2024-04-09 16:35:12 +0200

    user-profile-page need no user access to see page just for test

--- src/main/java/dev/ebrydeu/spring_boot_library/config/SecurityConfig.java
+++ src/main/java/dev/ebrydeu/spring_boot_library/config/SecurityConfig.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/home","/oauth2/authorization/github", "/api/public/**").permitAll()
+                        .requestMatchers("/", "/home","/oauth2/authorization/github","/user-profile-page", "/api/public/**").permitAll()
                         .requestMatchers("/api/user/**", "/user-profile-page").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
